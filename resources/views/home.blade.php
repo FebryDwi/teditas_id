21cf35ce8ff111aad3aeea6528298d71c45f3112
Febry Dwi <211221008@mhs.stiki.ac.id>
2023-08-15 12:56:53 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,370 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>TEDITAS - Teman Disabilitas</title>
+
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, maximum-scale=1"
+    />
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" href="css/aos.css" />
+    <link rel="stylesheet" href="css/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/templatemo-digital-trend.css" />
+  </head>
+  <body>
+    <!-- MENU BAR -->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.html"> TEDITAS. </a>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="#about" class="nav-link smoothScroll">Tentang</a>
+            </li>
+            <li class="nav-item">
+              <a href="#project" class="nav-link smoothScroll">Belajar</a>
+            </li>
+            <li class="nav-item">
+              <a
+                href="http://localhost/Website-TEDITAS/"
+                class="nav-link contact"
+                >Mulai</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- HOME -->
+    <section
+      class="hero hero-bg d-flex justify-content-center align-items-center"
+    >
+      <div class="container">
+        <div class="row">
+          <div
+            class="col-lg-6 col-md-10 col-12 d-flex flex-column justify-content-center align-items-center"
+          >
+            <div class="hero-text">
+              <h1 class="text-white" data-aos="fade-up">
+                Transkripsi Online Pengubah Suara Menjadi Teks Dalam Bahasa
+                Indonesia
+              </h1>
+
+              <a
+                href="http://localhost/Website-TEDITAS/"
+                class="custom-btn btn-bg btn mt-3"
+                data-aos="fade-up"
+                data-aos-delay="100"
+                >Coba Sekarang!</a
+              >
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-12">
+            <div class="hero-image" data-aos="fade-up" data-aos-delay="300">
+              <img src="images/chat.png" class="img-fluid" alt="working girl" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- TENTANG -->
+    <section class="about section-padding pb-0" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7 mx-auto col-md-10 col-12">
+            <div class="about-info">
+              <h2 class="mb-4" data-aos="fade-up" style="color: #04996c">
+                Solusi Alternatif Dalam Metode Pembelajaran
+              </h2>
+              <p class="mb-0" data-aos="fade-up">
+                <a href="index.html">TEDITAS (Teman Disabilitas)</a> Penerjemah
+                suara kedalam bentuk teks (Speech To Text) memungkinkan untuk
+                mengonversi kata-kata yang diucapkan menjadi teks. Dapat
+                meningkatkan partisipasi peserta didik di lingkungan pendidikan
+                dan membantu mereka untuk mengikuti perkembangan pembelajaran.
+              </p>
+              <br />
+              <p class="mb-0" data-aos="fade-up">
+                Selain itu, dengan menghilangkan hambatan dalam komunikasi,
+                peserta didik dengan disabilitas dapat merasa lebih termotivasi
+                dan percaya diri dalam mengikuti pembelajaran. Secara
+                keseluruhan, dalam penggunaannya di lingkungan pendidikan dapat
+                memberikan manfaat besar bagi peserta didik dengan disabilitas
+                dan meningkatkan kesetaraan dalam pendidikan.
+              </p>
+            </div>
+
+            <div class="about-image" data-aos="fade-up" data-aos-delay="200">
+              <img src="images/office.png" class="img-fluid" alt="office" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- BELAJAR -->
+    <section class="project section-padding" id="project">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-12 col-12">
+            <h2 class="mb-5 text-center" data-aos="fade-up">
+              Belajar bahasa isyarat bukan hanya tentang kata-kata,
+              <strong
+                >tetapi juga tentang memahami dan menghormati keberagaman dalam
+                berkomunikasi.</strong
+              >
+            </h2>
+
+            <div class="owl-carousel owl-theme" id="project-slide">
+              <div
+                class="item project-wrapper"
+                data-aos="fade-up"
+                data-aos-delay="100"
+              >
+                <img
+                  src="images/project/belajar.jpg"
+                  class="img-fluid"
+                  alt="project image"
+                />
+
+                <div class="project-info">
+                  <small>Video Belajar</small>
+                  <h3>
+                    <a href="https://www.youtube.com/watch?v=4DZnZv3weBw">
+                      <span>Belajar Bahasa Isyarat Untuk Pemula</span>
+                      <i class="fa fa-angle-right project-icon"></i>
+                    </a>
+                  </h3>
+                </div>
+              </div>
+
+              <div class="item project-wrapper" data-aos="fade-up">
+                <img
+                  src="images/project/belajar2.jpg"
+                  class="img-fluid"
+                  alt="project image"
+                />
+
+                <div class="project-info">
+                  <small>Video Belajar</small>
+
+                  <h3>
+                    <a
+                      href="https://www.youtube.com/watch?v=mukNGgweHSI&pp=ygUXYmVsYWphcmEgYmFoYXNhIGlzeWFyYXQ%3D"
+                    >
+                      <span>Bahasa Isyarat BISINDO</span>
+                      <i class="fa fa-angle-right project-icon"></i>
+                    </a>
+                  </h3>
+                </div>
+              </div>
+
+              <div class="item project-wrapper" data-aos="fade-up">
+                <img
+                  src="images/project/belajar3.jpg"
+                  class="img-fluid"
+                  alt="project image"
+                />
+
+                <div class="project-info">
+                  <small>Video Belajar</small>
+                  <h3>
+                    <a href="https://www.youtube.com/watch?v=h4hy75JWBdE">
+                      <span>Salam Dalam SIBI (Greetings)</span>
+                      <i class="fa fa-angle-right project-icon"></i>
+                    </a>
+                  </h3>
+                </div>
+              </div>
+
+              <div class="item project-wrapper" data-aos="fade-up">
+                <img
+                  src="images/project/belajar4.jpg"
+                  class="img-fluid"
+                  alt="project image"
+                />
+
+                <div class="project-info">
+                  <small>Video Belajar</small>
+                  <h3>
+                    <a href="https://www.youtube.com/watch?v=QUxNzUiWvAY">
+                      <span>Alfabet - Abjad dalam SIBI</span>
+                      <i class="fa fa-angle-right project-icon"></i>
+                    </a>
+                  </h3>
+                </div>
+              </div>
+
+              <div class="item project-wrapper" data-aos="fade-up">
+                <img
+                  src="images/project/belajar5.jpg"
+                  class="img-fluid"
+                  alt="project image"
+                />
+
+                <div class="project-info">
+                  <small>Video Belajar</small>
+                  <h3>
+                    <a href="https://www.youtube.com/watch?v=wC9R0Sw6QaY">
+                      <span>Belajar Bahasa Isyarat Bareng Surya Sahetapy</span>
+                      <i class="fa fa-angle-right project-icon"></i>
+                    </a>
+                  </h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- LOGO -->
+    <section class="testimonial section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="contact-image" data-aos="fade-up">
+              <img src="images/TUT.png" class="img-fluid" alt="website" />
+            </div>
+          </div>
+          <div class="col">
+            <div class="contact-image" data-aos="fade-up">
+              <img src="images/PKM23.png" class="img-fluid" alt="website" />
+            </div>
+          </div>
+          <div class="col">
+            <div class="contact-image" data-aos="fade-up">
+              <img
+                src="images/merdeka_belajar.png"
+                class="img-fluid"
+                alt="website"
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="contact-image" data-aos="fade-up">
+              <img
+                src="images/kampus_merdeka.png"
+                class="img-fluid"
+                alt="website"
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="contact-image" data-aos="fade-up">
+              <img
+                src="images/simbelmawa.png"
+                class="img-fluid"
+                alt="website"
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="contact-image" data-aos="fade-up">
+              <img src="images/STIKI.png" class="img-fluid" alt="website" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5 mx-lg-auto col-md-8 col-10">
+            <h1 class="text-white" data-aos="fade-up" data-aos-delay="100">
+              Be Creative & Professional
+            </h1>
+          </div>
+
+          <div
+            class="col-lg-3 col-md-6 col-12"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <h4 class="my-4">Informasi</h4>
+
+            <p class="mb-1">
+              <i class="fa fa-phone mr-2 footer-icon"></i>
+              (0341) 560823
+            </p>
+
+            <p>
+              <a href="#">
+                <i class="fa fa-instagram mr-2 footer-icon"></i>
+                @teditas_id
+              </a>
+            </p>
+          </div>
+
+          <div
+            class="col-lg-3 col-md-6 col-12"
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
+            <h4 class="my-4">Alamat</h4>
+            <p class="mb-1">
+              <i class="fa fa-home mr-2 footer-icon"></i>
+              Jl. Raya Tidar No.100, Karangbesuki, Kec. Sukun, Kota Malang, Jawa
+              Timur 65146
+            </p>
+          </div>
+
+          <div
+            class="col-lg-4 mx-lg-auto text-center col-md-8 col-12"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          ></div>
+
+          <!-- <div
+            class="col-lg-3 mx-lg-auto col-md-6 col-12"
+            data-aos="fade-up"
+            data-aos-delay="600"
+          >
+            <ul class="social-icon">
+              <li><a href="#" class="fa fa-instagram"></a></li>
+              <li><a href="#" class="fa fa-twitter"></a></li>
+              <li><a href="#" class="fa fa-dribbble"></a></li>
+              <li><a href="#" class="fa fa-behance"></a></li>
+            </ul>
+          </div> -->
+        </div>
+      </div>
+    </footer>
+
+    <!-- SCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
